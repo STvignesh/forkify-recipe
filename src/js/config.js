a991d8e5a05f9cded10d39beb941a336f965c459
STvignesh <stvignesh5@gmail.com>
2024-02-20 22:49:15 +0530

    API KEY

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes";
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = "c380fb9a-c1db-4461-ae87-36cfe04e32df";
+export const KEY = "2b9605b7-a286-43d9-80cd-8f2889940e46";
 export const MODAL_CLOSE_SEC = 2.5;
